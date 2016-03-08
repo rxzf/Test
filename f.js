5ace02842cdec80bf2c5602e3986923cd998b548
rxzf <1209601062@qq.com>
2016-03-08 09:39:26 +0800

    Added files via upload

---
+++ f.js
@@ -0,0 +1,17 @@
+var jiandao=document.getElementById("jiandao");
+jiandao.onclick=function()
+{
+document.getElementById("my_resuilt").src="jiandao.jpg";
+}
+
+var bu=document.getElementById("bu");
+bu.onclick=function()
+{
+document.getElementById("my_resuilt").src="bu.jpg";
+}
+
+var shitou=document.getElementById("shitou");
+shitou.onclick=function()
+{
+document.getElementById("my_resuilt").src="shitou.jpg";
+}
---
+++ new.php
@@ -0,0 +1,10 @@
+<script  src="js/a.js">
+</script>
+<div>
+<img id="shitou" src="shitou.jpg"onclick="s()";/>
+<img id="jiandao" src="jiandao.jpg"onclick="j()"; />
+<img id="bu" src="bu.jpg"onclick="b()";/>
+</div>
+<br/>
+<img id="my_resuilt"/>
+<img id="computer_resuilt"/>
