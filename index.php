9745cf41e69cb09eb3158b4c3d1fab67743bd149
rxzf <1209601062@qq.com>
2016-03-08 14:16:02 +0800

    Added files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-type=text/html; charset=utf8");
+?>
+<link rel="stylesheet"type="text/css"href="a.css">
+<div id="menue"><ul>
+    <li><a href="main1.php"target="mainye">test1 </a></li>
+        <li><a href="main2.php"target="mainye">test2</a></li>
+        <li><a href="main3.php"target="mainye">test3</a></li>
+        <li><a href="main4.php"target="mainye">test4</a></li>
+        <li><a href="main5.php"target="mainye">test5</a></li>
+    </ul>
+    </div>
+    <div id="main">
+        <iframe src="main1.php" name="mainye"frameborder="0" scrolling="no">
+        </div>
+<!--    <div id="left">-->
+<!--    <a href="main2"target="mainye">11</a>-->
+<!--    <a href="main3"target="mainye">22</a>-->
+<!--    </div>-->
+<!--<div id="main">-->
+<!--    <iframe src="main1.php" name="mainye">-->
+<!--</div>-->
+
